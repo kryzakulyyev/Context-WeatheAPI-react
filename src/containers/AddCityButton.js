f460927484a4dc454380272528ad0dfeef58aee6
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2021-01-28 21:00:20 -0800

    Weather Project

---
+++ src/containers/AddCityButton.js
@@ -0,0 +1,26 @@
+import React, {useContext, useState} from "react";
+import {WeatherContext} from '../context/WeatherContext'
+
+const AddCityButton = () => {
+
+   const [name, nameState] = useState('')
+
+  const {addCity} = useContext(WeatherContext)
+  const myKey ='8e03ef854788dde38bde26ecaf4db9c5'
+  const url = 'http://api.weatherstack.com/current?access_key=8e03ef854788dde38bde26ecaf4db9c5&query='
+  
+  
+    const realData = () =>{
+      fetch(`${url}${name}`).then(response => response.json())
+    .then(json => addCity(name,json.current.temperature))
+    }
+
+    return (
+      <div className="add-city-form">
+        <input className="input" value={name} onChange={(e)=>{nameState(e.target.value)}}/>
+        <button onClick={realData} className="input" >Add</button>
+      </div>
+    );
+  };
+
+  export default AddCityButton;
